add3c9dd15f053852b1c956c10a2ceef12f994a1
Rogel Gerodiaz <gerodiazrogel0@yahoo.com>
2025-09-03 18:19:36 +0800

    Remove mobile hamburger button from student sidebar

--- src/components/student/sidebar.php
+++ src/components/student/sidebar.php
@@ -33,12 +33,6 @@ $first_initial = strtoupper(substr($user_name, 0, 1));
             </svg>
         </button>
 
-        <!-- Hamburger for mobile, always visible for initial toggle -->
-        <button id="studentSidebarToggle" class="lg:hidden fixed top-4 left-4 z-50 bg-blue-600 text-white p-2 rounded-full shadow-lg focus:outline-none">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-        </button>
     </div>
 
     <!-- Navigation -->
